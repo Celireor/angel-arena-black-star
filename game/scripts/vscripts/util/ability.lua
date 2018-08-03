9102cc1fe3b879dcb34543af2347d7ea7c75cfa6
Celireor <32054822+Celireor@users.noreply.github.com>
2018-08-03 20:22:36 +0800

    Most abils prefer different targets anyway

--- game/scripts/vscripts/util/ability.lua
+++ game/scripts/vscripts/util/ability.lua
@@ -22,7 +22,7 @@ function CDOTABaseAbility:GetMulticastType()
 	-- 2 = ignite behavior (cast on different)
 	-- 3 = bloodlust behavior (instant cast)
 	if not self:HasBehavior(DOTA_ABILITY_BEHAVIOR_PASSIVE) and not table.contains(NOT_MULTICASTABLE_ABILITIES, self:GetAbilityName()) then
-		return MULTICAST_TYPE[self:GetAbilityName()] or 1
+		return MULTICAST_TYPE[self:GetAbilityName()] or 2
 	end
 	return false
 end
